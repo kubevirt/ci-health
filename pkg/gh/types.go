85081ea2e67ba38a16505f9e7e0f7f64859fe5de
Federico Gimenez <fgimenez@redhat.com>
2021-01-22 12:34:55 +0100

    added timeline items to query types

--- pkg/gh/types.go
+++ pkg/gh/types.go
@@ -2,19 +2,30 @@ package gh
 
 import "time"
 
-type PageInfo struct {
-	HasNextPage bool
-	EndCursor   string
+type LabeledEventFragment struct {
+	CreatedAt  time.Time
+	AddedLabel struct {
+		Name string
+	} `graphql:"addedLabel:label"`
 }
 
-type Comment struct {
-	CreatedAt time.Time
-	BodyText  string
+type UnlabeledEventFragment struct {
+	CreatedAt    time.Time
+	RemovedLabel struct {
+		Name string
+	} `graphql:"removedLabel:label"`
+}
+
+type TimeLineItem struct {
+	Nodes []struct {
+		LabeledEventFragment   `graphql:"... on LabeledEvent"`
+		UnlabeledEventFragment `graphql:"... on UnlabeledEvent"`
+	}
 }
 
 type PullRequestFragment struct {
-	Number   int
-	Comments struct {
-		Nodes []Comment
-	} `graphql:"comments(first: 100, orderBy: {field: UPDATED_AT, direction: DESC})"`
+	Number        int
+	TimeLineItems struct {
+		Nodes []TimeLineItem
+	} `graphql:"timeLineItems(first:100, itemTypes:[LABELED_EVENT, UNLABELED_EVENT])"`
 }
