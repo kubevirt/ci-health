ddd37a06de925711fa4c7c0a1fa64cf63fb1e7ba
Federico Gimenez <fgimenez@redhat.com>
2021-05-03 18:46:20 +0200

    initial batch command implementation

---
+++ cmd/batch/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/fgimenez/ci-health/pkg/constants"
+	"github.com/fgimenez/ci-health/pkg/runner"
+	"github.com/fgimenez/ci-health/pkg/types"
+)
+
+func main() {
+	opt := &types.Options{
+		Action: types.BatchAction,
+
+		Path:      constants.DefaultPath,
+		TokenPath: constants.DefaultTokenPath,
+		Source:    constants.DefaultSource,
+		DataDays:  constants.DefaultDataDays,
+		LogLevel:  constants.DefaultLogLevel,
+
+		TargetMetric: types.RetestsToMergeMetricType,
+		StartDate:    constants.DefaultBatchStartDate,
+	}
+
+	cmd := &cobra.Command{
+		Run: func(cmd *cobra.Command, arguments []string) {
+			_, err := runner.Run(opt)
+			if err != nil {
+				log.Fatalf("error: %v", err)
+			}
+			log.Println("Output written to", opt.Path)
+		},
+	}
+	flag := cmd.Flags()
+
+	tm := string(opt.TargetMetric)
+
+	flag.StringVar(&opt.Path, "path", opt.Path, "The directory to save results to.")
+	flag.StringVar(&opt.TokenPath, "gh-token", opt.TokenPath, "OAuth2 token to interact with GitHub API.")
+	flag.StringVar(&opt.Source, "source", opt.Source, "GitHub repo from where retrieve the data.")
+	flag.IntVar(&opt.DataDays, "data-days", opt.DataDays, "Number of days to retrieve data from.")
+	flag.StringVar(&opt.LogLevel, "log-level", opt.LogLevel, "Log level, valid values are debug and info.")
+
+	flag.StringVar(&tm, "target-metric", tm, "Which metric to generate.")
+	flag.StringVar(&opt.StartDate, "start-date", opt.StartDate, "From which do we start querying data in YYYY-MM-DD format, default .")
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+}
